b6f90ba1edafa305a7122b68bdec77354e0f9373
sai krishna neeli <saikrishnaneeli66@gmail.com>
2026-04-10 19:39:07 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style="whitegrid")
+plt.rcParams['figure.figsize'] = (7,4)
+
+df = pd.read_csv("bank_data.csv")
+
+df['job'] = df['job'].fillna('Unknown')
+df['education'] = df['education'].fillna('Unknown')
+
+bins = [18, 25, 35, 50, 65, 100]
+labels = ['18-25', '26-35', '36-50', '51-65', '65+']
+df['age_group'] = pd.cut(df['age'], bins=bins, labels=labels)
+
+print("Total Customers:", len(df))
+
+# ✅ Save plots instead of showing
+
+sns.countplot(x='deposit', data=df)
+plt.title("Deposit Subscription")
+plt.savefig("images/deposit_plot.png")
+plt.clf()
+
+sns.countplot(x='age_group', data=df)
+plt.title("Age Distribution")
+plt.savefig("images/age_distribution.png")
+plt.clf()
+
+sns.countplot(y='job', data=df)
+plt.title("Job Distribution")
+plt.savefig("images/job_distribution.png")
+plt.clf()
